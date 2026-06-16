513f9ee0329be2b79c9085b37bdb94d3c76411c5
Sujal Pawar <sujalpawar237@gmail.com>
2026-06-16 16:22:35 +0530

    Debug foodData route

--- mernapp/backend/Routes/DisplayData.js
+++ mernapp/backend/Routes/DisplayData.js
@@ -1,14 +1,11 @@
 
 const express = require("express");
 const router = express.Router();
-router.post('/foodData', (req, res) => {
-  try {
-    console.log(global.food_items);
-    res.send([global.food_items, global.foodCategory]);
-  } catch (error) {
-    console.error(error.message);
-    res.send("Internal Server Error");
-  }
+router.post("/foodData", (req, res) => {
+  console.log("food_items =", global.food_items?.length);
+  console.log("foodCategory =", global.foodCategory?.length);
+
+  res.json([global.food_items, global.foodCategory]);
 });
 
 module.exports = router;
